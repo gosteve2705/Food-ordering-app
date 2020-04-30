0740cfb336578f3cb73528625cc856809c3d3226
Nomuoja Oghenemaro <38343496+gosteve2705@users.noreply.github.com>
2020-04-30 02:49:29 +0100

    Add files via upload

---
+++ user.php
@@ -0,0 +1,20 @@
+<?php
+// Define the User class
+class User{
+  private $name;
+  private $gender;
+  
+  public function __construct($name,$gender){
+    $this->name=$name;
+    $this->gender=$gender;
+  }
+  
+  public function getName(){
+  return  $this->name;
+  }
+  public function getGender(){
+  return  $this->gender;
+  }
+}
+
+?>
